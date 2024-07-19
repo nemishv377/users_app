6ee90c3b9d321729b1da42c6949323a3827c10c4
nemishv.tagline@.com <nemishv.tagline@gmail.com>
2024-07-19 12:34:57 +0530

    starting with address model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,52 +3,6 @@ class User < ApplicationRecord
   belongs_to :state
   belongs_to :city
   serialize :hobbies, Array, coder: YAML
-  accepts_nested_attributes_for :state, :city
-  
-  # attr_accessor :state_name
-  # attr_accessor :city_name
-  # # attr_accessor :state_id
-  # before_save :update_state_attributes, :update_city_attributes
-
-  # def state_name
-  #   state.name if state
-  # end
-
-  # def state_name=(name)
-  #   @state_name = name
-  # end
-
-  # def city_name
-  #   city.name if city
-  # end
-
-  # def city_name=(name)
-  #   @city_name = name
-  # end
-
-  # # def city_state_id
-  # #   city.state_id if city
-  # # end
-
-  # # def city_state_id=(id)
-  # #   @city_state_id = id
-  # # end
-
-
-
-  # private
-  # def update_state_attributes
-  #   if state
-  #     state.update(state: @state_name)
-  #   end
-  # end
-
-  # def update_city_attributes
-  #   if city
-  #     city.update(city: @city_name)
-  #   end
-  # end
-
   
   VALID_GENDERS = ['Male', 'Female', 'Other']
   VALID_HOBBIES = ['reading','travelling','photography']
@@ -63,7 +17,7 @@ class User < ApplicationRecord
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
   validates :gender, presence: true, inclusion: { in: VALID_GENDERS, message: "%{value} is not a valid gender" }
   validates :hobbies, presence: true, inclusion: { in: VALID_HOBBIES, message: "%{value} is not a valid Hobby." }
-  validates :state, presence: true
-  validates :city, presence: true
+  validates :state, presence: true, inclusion: { in: VALID_STATES, message: "%{value} is not a valid State." }
+  validates :city, presence: true, inclusion: { in: VALID_CITIES, message: "%{value} is not a valid City." }
 
 end
