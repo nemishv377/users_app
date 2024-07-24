72c37409e32d47cd224b32a3cb9400a26d477613
nemishv.tagline@.com <nemishv.tagline@gmail.com>
2024-07-24 18:44:54 +0530

    crud for address association done

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,14 @@ class User < ApplicationRecord
   VALID_HOBBIES = ['reading','travelling','photography']
   VALID_STATES = State.all
   VALID_CITIES = City.all
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   
   validates :first_name, presence: true, length: { minimum: 2, maximum: 50, message: "must be between 2 and 50 characters" },
                          format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
   validates :last_name, presence: true, length: { minimum: 2, maximum: 50, message: "must be between 2 and 50 characters" },
                          format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :gender, presence: true, inclusion: { in: VALID_GENDERS, message: "%{value} is not a valid gender" }
   validates :hobbies, presence: true, inclusion: { in: VALID_HOBBIES, message: "%{value} is not a valid Hobby." }
   validates :state, presence: true, inclusion: { in: VALID_STATES, message: "%{value} is not a valid State." }
