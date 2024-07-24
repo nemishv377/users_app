2f26c3bcb61121ab5bf362088ba039a7f0aac444
nemishv.tagline@.com <nemishv.tagline@gmail.com>
2024-07-24 16:28:52 +0530

    done crud operation nested asscoiation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :user
 
-  validates :plot_no, presence: true, format: { with:  /\A[a-zA-Z0-9]+\z/, message: "only allows alphanumeric characters" }
-  validates :society_name, presence: true
-  validates :pincode, presence: true, length: { is: 6 }, format: { with: /\A\d{6}\z/, message: "should be 6 digits long" }
+  validates :plot_no, presence: { message: "Plot no can't be blank." }, format: { with:  /\A[a-zA-Z0-9]+\z/, message: "Plot no only allows alphanumeric characters" }
+  validates :society_name, presence: { message: "Society name can't be blank." }
+  validates :pincode, presence: { message: "Pincode can't be blank." }, format: { with: /\A\d{6}\z/, message: "Pincode should be 6 digits long" }, length: { is: 6 }
 end
