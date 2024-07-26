92fdf3fa78bbe361ae15881c077b27ce183b93a5
nemishv.tagline@.com <nemishv.tagline@gmail.com>
2024-07-26 18:42:45 +0530

    made one addreess as default address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_26_083520) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_26_105211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_26_083520) do
     t.datetime "updated_at", null: false
     t.bigint "state_id", null: false
     t.bigint "city_id", null: false
+    t.boolean "default"
     t.index ["city_id"], name: "index_addresses_on_city_id"
     t.index ["state_id"], name: "index_addresses_on_state_id"
     t.index ["user_id"], name: "index_addresses_on_user_id"
