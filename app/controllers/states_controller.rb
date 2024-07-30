37130f7a028bf19f12f3f856a8cb6fc1de619f58
nemishv.tagline@.com <nemishv.tagline@gmail.com>
2024-07-30 15:06:14 +0530

    n+1 query problem solved

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,7 +1,6 @@
 class StatesController < ApplicationController
   def cities
     @cities = City.where(state_id: params[:id])
-    puts @cities
     render json: @cities
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,14 @@ class User < ApplicationRecord
   accepts_nested_attributes_for :addresses, allow_destroy: true
   
   VALID_GENDERS = ['Male', 'Female', 'Other']
-  VALID_HOBBIES = ['reading','travelling','photography']
+  VALID_HOBBIES = ['Reading','Travelling','Photography']
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :first_name, presence: true, length: { minimum: 2, maximum: 50, message: "must be between 2 and 50 characters" },
-                         format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
-  validates :last_name, presence: true, length: { minimum: 2, maximum: 50, message: "must be between 2 and 50 characters" },
-                         format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  validates :first_name, presence: true, length: { minimum: 2, maximum: 50, message: "must be between 2 and 50 characters" }
+  validates :last_name, presence: true, length: { minimum: 2, maximum: 50, message: "must be between 2 and 50 characters" }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :gender, presence: true, inclusion: { in: VALID_GENDERS, message: "%{value} is not a valid gender" }
-  validates :hobbies, presence: true, inclusion: { in: VALID_HOBBIES, message: "%{value} is not a valid Hobby." }
+  validates :gender, presence: true
+  validates :hobbies, presence: true
   validate :profile_avatar_content_type
 
   def profile_avatar_content_type
@@ -22,6 +20,4 @@ class User < ApplicationRecord
       errors.add(:avatar, 'must be a JPEG or PNG')
     end
   end
-
-
 end
