4440d505ff19b53c6ce5d78f5ca629eb29cde1d5
nemishv.tagline@.com <nemishv.tagline@gmail.com>
2024-08-01 15:40:26 +0530

    using bullet gem

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ gem "tzinfo-data", platforms: %i[ windows jruby ]
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
 gem 'pry-rails'
+gem 'bullet', group: 'development'
 # gem "jquery-rails"
 # gem "sassc-rails"
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  #
+  # If you remove below code then it will alert for eager loading
+  # If Not used eger loading. 
+  # around_action :skip_bullet, if: -> { defined?(Bullet) }
+
+  # def skip_bullet
+  #   previous_value = Bullet.enable?
+  #   Bullet.enable = false
+  #   yield
+  # ensure
+  #   Bullet.enable = previous_value
+  # end
 end
