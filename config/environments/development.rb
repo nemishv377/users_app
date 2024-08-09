78b34c24c6edf6762cd85530b375ac14e9ea4723
nemishv.tagline@.com <nemishv.tagline@gmail.com>
2024-08-09 14:11:31 +0530

    reduce the no. of quries using bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
     Bullet.console       = true
     Bullet.rails_logger  = true
     Bullet.add_footer    = true
-    Bullet.add_safelist :type => :unused_eager_loading, :class_name => "User", :association => :addresses
+    # Bullet.add_safelist :type => :unused_eager_loading, :class_name => "User", :association => :addresses
   end
 
   # Settings specified here will take precedence over those in config/application.rb.
