5a19bd64a4c79d877113a4442ee23c519da36d2a
nemishv.tagline@.com <nemishv.tagline@gmail.com>
2024-08-13 18:49:22 +0530

    error in sending mail to new user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -85,4 +85,20 @@ Rails.application.configure do
   config.action_controller.raise_on_missing_callback_actions = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # config.action_mailer.delivery_method = :sendmail
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_options = { from: 'nemishv.tagline@gmail.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'example.com',
+    user_name: 'nemishv.tagline@gmail.com',
+    password: '25mkcgUT3u$!',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
