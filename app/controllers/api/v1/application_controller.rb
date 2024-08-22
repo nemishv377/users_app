5c942ac6437caf5bc7471db2c18a9f64ed9ebba6
nemishv.tagline@.com <nemishv.tagline@gmail.com>
2024-08-22 18:48:45 +0530

    create api for change password

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -7,24 +7,18 @@ module Api
 
       before_action :authenticate_user_from_token!
 
-      rescue_from CanCan::AccessDenied do |_exception|
-        redirect_to users_path, alert: 'You are not authorized to access that page.'
-      end
-
-      private
-
       def authenticate_user_from_token!
         token = request.headers['Authorization']&.split(' ')&.last
 
         if token.present?
           decoded_token = JwtToken.decode(token)
           if decoded_token.nil?
-            render json: { errors: 'Invalid or missing token' }, status: :unauthorized
+            render json: { errors: 'Invalid token' }, status: :unauthorized
           elsif decoded_token
             @current_user = User.find(decoded_token[:user_id])
           end
         else
-          render json: { error: 'Not Authorized' }, status: :unauthorized
+          render json: { error: 'Token missing...' }, status: :unauthorized
         end
       rescue ActiveRecord::RecordNotFound => e
         render json: { errors: e.message }, status: :unauthorized
