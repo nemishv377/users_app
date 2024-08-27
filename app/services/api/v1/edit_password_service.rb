4dc87e2bb41bfdd864db70b01e8dddcbc5f5ea4c
nemishv.tagline@.com <nemishv.tagline@gmail.com>
2024-08-27 16:34:57 +0530

    services using business process

--- app/services/api/v1/edit_password_service.rb
+++ app/services/api/v1/edit_password_service.rb
@@ -1,20 +1,18 @@
 module Api
   module V1
-    class EditPasswordService
-      def initialize(reset_password_token, password)
-        @reset_password_token = reset_password_token
-        @password = password
-      end
+    class EditPasswordService < BusinessProcess::Base
+      needs :reset_password_token
+      needs :password
 
       def call
-        return { message: 'Token and password both are missing.' } if @password.blank? && @reset_password_token.blank?
-        return { message: 'Password is required.' } if @password.blank?
-        return { message: 'Token is required.' } if @reset_password_token.blank?
+        return { message: 'Token and password both are missing.' } if password.blank? && reset_password_token.blank?
+        return { message: 'Password is required.' } if password.blank?
+        return { message: 'Token is required.' } if reset_password_token.blank?
 
-        user = User.find_by(reset_password_token: @reset_password_token)
+        user = User.find_by(reset_password_token:)
         return { error: 'Invalid or expired token' } if user.nil?
 
-        if user.update(password: @password, reset_password_token: nil)
+        if user.update(password:, reset_password_token: nil)
           { user:, message: 'Password successfully updated' }
         else
           { error: 'Unable to update password' }
